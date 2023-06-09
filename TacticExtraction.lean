5a5b00a96fffab0f62469a319985eeb95d46611a
ART0 <18333981+0Art0@users.noreply.github.com>
2023-06-09 15:06:57 +0530

    Counting mechanism in output

--- TacticExtraction.lean
+++ TacticExtraction.lean
@@ -9,5 +9,11 @@ def main : IO Unit := do
 
   let files := (← System.FilePath.walkDir "./lake-packages/mathlib/Mathlib").filter (·.extension = some "lean")
   let tasks ← files.mapM (IO.asTask <| LeanInk.Analysis.execAux ·.toString)
+  
+  let mut count := 0
+  let total := files.size
+  
   for task in tasks do
+    IO.println s!"File {count} out of {total}."
     let _ ← IO.wait task
+    count := count + 1
