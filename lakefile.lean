a6f8ce1470df3a72b302579bc0fe256768eb3688
ART0 <18333981+0Art0@users.noreply.github.com>
2023-06-09 11:01:15 +0530

    Clean `lakefile`

--- lakefile.lean
+++ lakefile.lean
@@ -4,14 +4,10 @@ open System Lake DSL
 open System.FilePath IO IO.FS
 
 package leanInk where
-  moreLeanArgs := #["-Dtactic.simp.trace=true"]
-  moreServerArgs := #["-Dtactic.simp.trace=true"]
 
 lean_lib LeanInk where
-  moreLeanArgs := #["-Dtactic.simp.trace=true"]
 
 @[default_target]
 lean_exe leanInk where
   root := `LeanInk
   supportInterpreter := true
-  moreLeanArgs := #["-Dtactic.simp.trace=true"]
