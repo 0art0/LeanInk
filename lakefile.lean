edad175ebc1e24b0b02cd96011cb84fa166aea34
ART0 <18333981+0Art0@users.noreply.github.com>
2023-06-09 17:08:45 +0530

    `supportInterpreter` in `lakefile`

--- lakefile.lean
+++ lakefile.lean
@@ -16,6 +16,7 @@ lean_exe leanInk where
 @[default_target]
 lean_exe tacticExtraction where
   root := `TacticExtraction
+  supportInterpreter := true
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"@"master"
