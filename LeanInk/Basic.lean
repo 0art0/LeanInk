80ce8a4f134ba5cd5723a07a49126690c8ca2f24
ART0 <18333981+0Art0@users.noreply.github.com>
2023-06-09 15:40:38 +0530

    Reorganise `tacticExtraction`

--- LeanInk/Basic.lean
+++ LeanInk/Basic.lean
@@ -73,3 +73,19 @@ def exec : List String -> IO UInt32
       if (← execAux file) != 0 then
         return ← Logger.logError s!"Analysis for \"{file}\" failed!"
     return 0
+
+def mathlibExtract (dir : FilePath := "./lake-packages/mathlib/Mathlib/") : IO UInt32 := do
+  enableInitializersExecutionWrapper
+
+  if !(← System.FilePath.pathExists "TacticExtractionData") then  
+    IO.FS.createDir "TacticExtractionData"
+
+  let files := (← System.FilePath.walkDir dir).filter (·.extension = some "lean")
+  let tasks ← files.mapM (IO.asTask <| LeanInk.Analysis.execAux ·.toString)
+
+  LeanInk.Analysis.enableInitializersExecutionWrapper
+
+  for task in tasks do
+    if let .error e := (← IO.wait task) then throw e
+  
+  return 0
--- TacticExtraction.lean
+++ TacticExtraction.lean
@@ -3,17 +3,4 @@ import LeanInk.Logger
 
 /-! Code for tactic step data extraction from `Mathlib`. -/
 
-def main : IO Unit := do
-  if !(← System.FilePath.pathExists "TacticExtractionData") then  
-    IO.FS.createDir "TacticExtractionData"
-
-  let files := (← System.FilePath.walkDir "./lake-packages/mathlib/Mathlib").filter (·.extension = some "lean")
-  let tasks ← files.mapM (IO.asTask <| LeanInk.Analysis.execAux ·.toString)
-  
-  let mut count := 0
-  let total := files.size
-  
-  for task in tasks do
-    IO.println s!"File {count} out of {total}."
-    let _ ← IO.wait task
-    count := count + 1
+def main : IO UInt32 := LeanInk.Analysis.mathlibExtract
