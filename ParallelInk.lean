6bd9d9d97450bb7fd8a2df5186089247f4b6e844
ART0 <18333981+0Art0@users.noreply.github.com>
2023-06-10 11:05:45 +0530

    Update `ParallelInk` to take command line arguments

--- ParallelInk.lean
+++ ParallelInk.lean
@@ -2,14 +2,18 @@ import Lean
 
 open System IO IO.Process
 
-def main : IO Unit := do
+def main (args : List String) : IO Unit := do
   if !(← FilePath.pathExists "TacticExtractionData") then  
     FS.createDir "TacticExtractionData"
 
-  let dir := "./lake-packages/mathlib/Mathlib/ModelTheory"
-  let files := (← FilePath.walkDir dir).filter (·.extension = some "lean")
-  let tasks : Array (Child {}) ← files.mapM (spawn { cmd := "./build/bin/leanInk", args := #[·.toString] })
-  for (file, task) in (Array.zip files tasks) do
-    if (← task.wait) != 0 then
-      IO.println s!"Error in processing {file.toString}."
+  let [path] := args | IO.throwServerError "The `parallelInk` script takes only a single argument."
+  let path : FilePath := "lake-packages/mathlib/Mathlib/" ++ path
+  if ← FilePath.isDir path then
+    let files := (← FilePath.walkDir path).filter (·.extension = some "lean")
+    let tasks : Array (Child {}) ← files.mapM (spawn { cmd := "./build/bin/leanInk", args := #[·.toString] })
+    for (file, task) in (Array.zip files tasks) do
+      if (← task.wait) != 0 then
+        IO.println s!"Error in processing {file.toString}."
+  else IO.println <| ← run { cmd := "./build/bin/leanInk", args := #[path.toString]}
+
   IO.println "\n\n Tactic extraction complete." 
