d9013b0466149202f555ab7945871facdc337f2b
ART0 <18333981+0Art0@users.noreply.github.com>
2023-06-18 12:02:31 +0530

    Improve Python tactic extraction script

--- tactic_extraction.py
+++ tactic_extraction.py
@@ -1,19 +1,24 @@
 import os
 import fnmatch
+import sys
 import subprocess
 import concurrent.futures
 
-# Written with help from ChatGPT
+"""
+This script attempts to run the tactic extraction script (`./build/bin/leanInk`) over all files of `Mathlib` in parallel.
+Parts of this code were written with help from ChatGPT.
+"""
 
 def find_lean_files(directory):
-    lean_files = []
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-            if fnmatch.fnmatch(file, '*.lean'):
-                lean_files.append(os.path.join(root, file))
-    return lean_files
+    """Finds all `.lean` files in the specified directory (including sub-folders)."""
+    return [os.path.join(root, file) 
+        for root, dirs, files in os.walk(directory) 
+        for file in files 
+            if fnmatch.fnmatch(file, "*.lean")]
 
-repository_path = './lake-packages/mathlib/Mathlib/'
+repository_path = "./lake-packages/mathlib/Mathlib/" # the location of the `Mathlib` source code
+folder_path = "CategoryTheory/Functor" # can be modified to a specific sub-folder of `Mathlib`, like `Combinatorics/Regularity` or `Data/Int`
+num_workers = 3 # the maximum number of workers in the concurrent code
 
 def process_file(file):
     command = ['./build/bin/leanInk', file]
@@ -23,6 +28,6 @@ def process_file(file):
     except subprocess.CalledProcessError as e:
         print(f"Error executing command on {file}: {e}")
 
-# Use concurrent.futures for concurrent execution
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    executor.map(process_file, find_lean_files(repository_path))
+# Concurrent execution of the `leanInk` script as *processes* (not *threads*) using the `concurrent.futures` framework
+with concurrent.futures.ProcessPoolExecutor(max_workers=num_workers) as executor:
+   executor.map(process_file, find_lean_files(repository_path + folder_path))
