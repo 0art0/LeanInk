400fc57ef363cb2e18eee157f5e43d7cf0fc267c
ART0 <18333981+0Art0@users.noreply.github.com>
2023-06-29 16:43:19 +0530

    Polished script

--- Scripts/DependencyGen.lean
+++ Scripts/DependencyGen.lean
@@ -9,16 +9,23 @@ def createMathlibEnv : IO Environment := do
   initSearchPath (← getBuildDir) ["build/lib", "lake-packages/mathlib/build/lib/",  "lake-packages/std/build/lib/", "lake-packages/Qq/build/lib/", "lake-packages/aesop/build/lib/", "lake-packages/proofwidgets/build/lib/"]
   importModules [{module := `Init}, {module := `Mathlib}] .empty
 
+def isValidMathlibConstant (mathlibEnv : Environment) (nm : Name) : Bool := Option.toBool (α := Unit) <| do
+  guard <| ! nm.isImplementationDetail
+  guard <| ! nm.isInaccessibleUserName
+  guard <| ! nm.isInternal 
+  let mod ← mathlibEnv.getModuleNameFor? nm
+  guard <| (`Mathlib).isPrefixOf mod
+  return ()
+
 def mathlibDepsOf (mathlibEnv : Environment) (nm : Name) : IO <| Array (Name × Name) := do  
   let some constinfo := mathlibEnv.constants.find? nm | IO.throwServerError s!"The constant {nm} is not in the `Mathlib` environment."
   let some term := constinfo.value? | do
     IO.println s!"No value found for the term {nm}."
     return #[]
   return term.foldConsts #[] <| fun c consts ↦
-    if let some mod := mathlibEnv.getModuleNameFor? c then
-      if (`Mathlib).isPrefixOf mod then
-        consts.push (mod, c)
-      else consts
+    if isValidMathlibConstant mathlibEnv c then
+      -- the `get!` is justified because the declaration is in `Mathlib`
+      consts.push (mathlibEnv.getModuleNameFor? c |>.get!, c)
     else consts
 
 def main : IO Unit := do
@@ -33,10 +40,10 @@ def main : IO Unit := do
     IO.println s!"Created folder {folder} for data extraction."
 
   let tasks ← env.constants.toList.mapM <| fun (nm, _) ↦ IO.asTask <| do
-    if (`Mathlib).isPrefixOf <$> (env.getModuleNameFor? nm) == some true then
+    if isValidMathlibConstant env nm then
       let deps ← mathlibDepsOf env nm
       let data := deps.foldl (init := "") <| fun s (mod, dep) ↦ s ++ s!"{mod}, {dep}\n"
-      IO.FS.writeFile (folder / nm.toString) data
+      IO.FS.writeFile (folder / (nm.toString ++ ".txt")) data
       IO.println s!"Written data for {nm}."
 
   for task in tasks do
